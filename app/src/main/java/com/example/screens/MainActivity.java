ad29ded8a30b4518cd946ab8f00aeea0fc7ae5da
a22fokol <a22fokol@student.his.se>
2023-05-02 11:35:46 +0200

    Jag har skapat Mainactivity 2 och har skapat button och textview på det MainActivity

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.screens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button sendButton = findViewById(R.id.sendButton);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,  MainActivity.class);
+
+                EditText nameEdit = findViewById(R.id.editTextName);
+                String name = nameEdit.getText().toString();
+
+                intent.putExtra("name", name); // Optional
+                startActivity(intent);
+            }
+        });
     }
 }
+
---
+++ app/src/main/java/com/example/screens/MainActivity2.java
@@ -0,0 +1,26 @@
+package com.example.screens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            String name = extras.getString("name");
+
+            // Do something with the name and number
+
+
+            TextView nameView = findViewById(R.id.textViewName);
+            nameView.setText(name);
+        }
+    }
+}
